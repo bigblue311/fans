8c133d0432f343486d773966a95318191537491d
victor@MAC <victor_jsp@126.com>
2016-02-27 16:35:12 +0800

    样式调整

--- fans.app.web/src/main/java/com/fans/admin/model/form/UserQueryFormBean.java
+++ fans.app.web/src/main/java/com/fans/admin/model/form/UserQueryFormBean.java
@@ -5,11 +5,19 @@ import com.victor.framework.common.tools.StringTools;
 
 
 public class UserQueryFormBean extends QueryFormBean{
+    private Long id;
     private String nickName;
     private String phone;
     private String province;
     private String city;
     private int page = 1;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getNickName() {
         return nickName;
     }
@@ -43,6 +51,9 @@ public class UserQueryFormBean extends QueryFormBean{
     
     public UserQueryCondition toQuery(){
         UserQueryCondition queryCondition = new UserQueryCondition();
+        if(id!=null){
+            queryCondition.setId(id);
+        }
         if(StringTools.isNotEmpty(nickName)){
             queryCondition.setNickName(nickName);
         }
